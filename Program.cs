aca0a4f5c19d4cc8b0e4a759e9308ac2c8a51bee
boushib <boushib01@gmail.com>
2020-03-28 23:26:22 +0100

    created a starter C# project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int age = 29;
+      age--;
+      Console.WriteLine(age);
+    }
+  }
+}
